3acd72c99609e8c908d4921092da95a5c2f08d3b
Aluno <Aluno@LabCVT-PC13>
2018-11-07 10:39:02 -0200

    07-11-2018

---
+++ src/Neuronio.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Aluno
+ */
+public class Neuronio {
+    private int x[];//entrada associada ao peso
+    private double w;//pesos (novo e atual)
+    private int y;//saida do neuronio
+    private double e;//erro
+    private double n;//taxa de aprendizagem 
+
+   public void Neuronio (int qtdEntradas){
+       
+   }
+    
+   public void inicializaPesos (){
+     w = 0;
+     
+   } 
+   
+   public void setEntradas (int indice, int valor){
+       
+   }
+   
+   public void calculaSaidaNeuronio (){
+       
+   }
+   public void setErro (int yd){
+       
+   }
+   
+   public void atualizaPeso (){
+       
+   }
+   
+   public double getErro (){
+        return 0;
+       
+   }
+   
+   public double getSaida (){
+        return 0;
+       
+   }
+   
+   
+    
+}
---
+++ src/Rede.java
@@ -0,0 +1,41 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Aluno
+ */
+
+public class Rede {
+
+    private int qtdAtributos;
+    private int qtdPadroes;
+    private Neuronio mcp;
+    private int padroes[][];
+    private int saidaDesejada[];
+    private Scanner entrada;
+    
+    public void iniciaRede(){
+       int [][] padroes = {{0,0}, {0,1}};
+    }
+    
+    public void treinaRede(){
+        
+    }
+    
+    public void testarRede(){
+        
+    }
+    
+            
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
