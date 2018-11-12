e4976c1b09489fd051c968e795d7c9b7215caf7f
Aluno <airis.vital@hotmail.com>
2018-11-12 11:55:16 -0300

    12/11/2018

--- src/Rede.java
+++ src/Rede.java
@@ -1,28 +1,19 @@
-
 import java.util.Scanner;
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author Maria Iris
  */
-
 public class Rede {
 
     private int qtdAtributos;
     private int qtdPadroes;
     private Neuronio mcp;
-    private int padroes[][];
-    private int saidaDesejada[];
-    private Scanner entrada;
+    //private int padroes[][];
+    //private int saidaDesejada[];
+    private Scanner entrada = new Scanner(System.in);;
     
     public void iniciaRede(){
-       //Scanner entrada = new Scanner(System.in);
+      
        System.out.print("Informe os numeros de PADROES:");
        qtdPadroes = entrada.nextInt();
        System.out.print("Informe os numeros de ATRIBUTOS:");
@@ -32,42 +23,52 @@ public class Rede {
     }
     
     public void treinaRede(){
-        //int treinamento = 1;
-        //double erro;
-        
-        //padroes[4][6] = {{1,0,1,0}, {0,1,0,1},{1,0,0,1},{0,1,1,1},{0,0,1,1},{0,0,0,1}};
-        
         double erro;
-		int iteracao = 1;
-		System.out.println("--------------------------------");
-		System.out.println("TREINANDO REDE NEURAL PERCEPTRON");
-		System.out.println("--------------------------------");
-		do {
-			System.out.println("ITERACAO " + iteracao++);
-			erro = 0;
-			for (int i = 0; i < qtdPadroes; i++) {//Mostra todos os padroes
-				System.out.println("ENTRE COM OS ATRIBUTOS DO PADRAO "+i);
-				for (int j = 0; j < qtdAtributos; j++) {//Mostra os atributos de cada padrao
-					mcp.setEntrada(j, entrada.nextInt());//indice e valor da entrada do neuronio
-				}
-				System.out.println("ENTRE COM A SAIDA DESEJADA DO PADRAO " + i);
-				mcp.setSaidaDesejada(entrada.nextInt());
-				mcp.calcSaidaNeuronio();
-				mcp.setErro();
-				erro = erro + Math.abs(mcp.getErro()); //TODO valor absoluto de getErro
-			}
-		} while (erro > 0);
-		System.out.println("--------------------------------");
-
+        int treinamento = 1; 
+        
+        //padroes[][] = {{1,0,1,0}, {0,1,0,1},{1,0,0,1},{0,1,1,1},{0,0,1,1},{0,0,0,1}};
+        
+        
+        System.out.println("Treinamento da Rede Neural:");
+        
+        do {
+            System.out.println("Treinamento " + treinamento++);
+            erro = 0;
+                for (int i = 0; i < qtdPadroes; i++) {//Mostra todos os padroes
+                    System.out.println("Digite os ATRIBUTOS"+i);
+                    for (int j = 0; j < qtdAtributos; j++) {// os atributos de cada padrao
+                        mcp.setEntradas(j, entrada.nextInt());//indice e valor do neuronio
+                    }
+                    System.out.println("Digite as saídas desejadas do PADRAO " + i);
+                    mcp.setSaidaDesejada(entrada.nextInt());
+                    mcp.calculaSaidaNeuronio();
+                    mcp.setErro();
+                    
+                    erro = erro + Math.abs(mcp.getErro()); //valor de getErro
+                }
+        } while (erro > 0);
     }
     
     public void testarRede(){
-        
-    }
+        do {
+            System.out.println("EXECUTANDO REDE NEURAL PERCEPTRON");
+            System.out.println("--------------------------------");
+            System.out.println("Digite os ATRIBUTOS do PADRAO");
+            for (int j = 0; j < qtdAtributos; j++) {
+                mcp.setEntradas(j, entrada.nextInt());
+            }
+            mcp.calculaSaidaNeuronio();
+            System.out.println("Padrão que pertence a classe: " + mcp.getSaida());
+            //System.out.println("DIGITE 9 PARA CONTINUAR");
+	} while (entrada.nextInt() == 9); //9
+	
+	}
     
-            
     public static void main(String[] args) {
-        // TODO code application logic here
+        Rede perceptron = new Rede();
+	perceptron.iniciaRede();
+	perceptron.treinaRede();
+	perceptron.testarRede();
     }
     
 }
